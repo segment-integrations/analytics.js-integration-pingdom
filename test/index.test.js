e624a3cc73d58ec3e173e8708b72adc245844ca1
Nathan Houle <nathan@nathanhoule.com>
2015-06-09 16:03:46 -0700

    Initial commit

---
+++ test/index.test.js
@@ -0,0 +1,76 @@
+
+var Analytics = require('analytics.js').constructor;
+var integration = require('analytics.js-integration');
+var tester = require('analytics.js-integration-tester');
+var date = require('load-date');
+var sandbox = require('clear-env');
+var Pingdom = require('../lib/');
+
+describe('Pingdom', function() {
+  var analytics;
+  var pingdom;
+  var options = {
+    id: '5168f8c6abe53db732000000'
+  };
+
+  beforeEach(function() {
+    analytics = new Analytics();
+    pingdom = new Pingdom(options);
+    analytics.use(Pingdom);
+    analytics.use(tester);
+    analytics.add(pingdom);
+  });
+
+  afterEach(function() {
+    analytics.restore();
+    analytics.reset();
+    pingdom.reset();
+    sandbox();
+  });
+
+  it('should have the right settings', function() {
+    analytics.compare(Pingdom, integration('Pingdom')
+      .assumesPageview()
+      .global('_prum')
+      .global('PRUM_EPISODES')
+      .option('id', ''));
+  });
+
+  describe('before loading', function() {
+    beforeEach(function() {
+      analytics.stub(pingdom, 'load');
+    });
+
+    describe('#initialize', function() {
+      it('should call #load', function() {
+        analytics.initialize();
+        analytics.page();
+        analytics.called(pingdom.load);
+      });
+
+      it('should push the id onto window._prum', function() {
+        analytics.initialize();
+        analytics.page();
+        analytics.deepEqual(window._prum[0], ['id', options.id]);
+      });
+    });
+  });
+
+  describe('loading', function() {
+    it('should load', function(done) {
+      analytics.load(pingdom, done);
+    });
+  });
+
+  describe('after loading', function() {
+    beforeEach(function(done) {
+      analytics.once('ready', done);
+      analytics.initialize();
+      analytics.page();
+    });
+
+    it('should mark the first byte', function() {
+      analytics.equal(date.getTime(), window.PRUM_EPISODES.marks.firstbyte);
+    });
+  });
+});
