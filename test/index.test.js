c7bb96f81d02f91ec4f459145568e3a628682b88
Peter Richmond <peter.h.richmond@gmail.com>
2017-04-07 10:58:06 -0700

    Pin karma, karma-mocha dev dependencies (#3)
    Karma is failing due to some after-effects, but all tests are operating as intended. Merging.

--- test/index.test.js
+++ test/index.test.js
@@ -14,6 +14,8 @@ describe('Pingdom', function() {
     id: '5168f8c6abe53db732000000'
   };
 
+  function noop() {}
+
   beforeEach(function() {
     analytics = new Analytics();
     pingdom = new Pingdom(options);
@@ -36,7 +38,7 @@ describe('Pingdom', function() {
   // the events but no dice on IE, so this hax is easiest.
   after(function() {
     window.PRUM_EPISODES = {
-      onUnload: function() {}
+      onUnload: noop()
     };
   });
 
